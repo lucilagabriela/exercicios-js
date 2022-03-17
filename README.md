bd77de0d183fc7bcfed745ad05a55eae243e089a
[lucilagabriela] <lucilagdac@gmail.com>
2022-03-16 22:31:44 -0300

    up

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # exercicios-js
  
+- [ ] https://github.com/fernandoleonid/mini-projetos-js/blob/master/00-imc/imc.js
--- imc/script.js
+++ imc/script.js
@@ -1,14 +1,14 @@
 const botaoIMC = document.getElementById('buttonIMC');
 
 function calcular() {
-    const name = document.getElementById('name').value;
+    const nome = document.getElementById('name').value;
     const altura = document.querySelector('p#altura').value;
     const peso = document.querySelector('p#peso').value;
     const resultado = document.getElementById('resultado');
 
-    if (name !== '' && altura !== '' && peso !== '') {
+    if (nome !== '' && altura !== '' && peso !== '') {
         const valorIMC = (peso/(altura*altura));
-        resultado.textContent = `${name}, ${valorIMC}`;
+        resultado.textContent = `${nome}, ${valorIMC}`;
     } else {
         alert('preencha todos os dados!');
     }
